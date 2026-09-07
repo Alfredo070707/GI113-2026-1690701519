1717ad7d02b5004f37f48e4151d635777cc23f6d
Alfredo070707 <madosukiz2550@gmail.com>
2026-09-07 10:40:44 +0700

    Update Program.cs

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -13,38 +13,7 @@ namespace Lab04
         static void Main(string[] args)
         {
             Console.WriteLine("+------------------------+");
-            Console.WriteLine("|      NEW ADVENTURE      ");
-            Console.WriteLine("+------------------------+");
-            Console.Write("Name your hero: ");
-            string playerName = Console.ReadLine();
-            Console.WriteLine($"\n\"Welcome, {playerName}. Your journey begins...\"");
-
-            
-            Console.WriteLine("+------------------------+");
-            Console.WriteLine("|      DIFFICULTY         ");
-            Console.WriteLine("+------------------------+");
-            Console.Write("Choose difficulty (1-3): ");
-            int difficulty = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Difficulty set to {difficulty}");
-
-            Console.WriteLine("+------------------------+");
-            Console.WriteLine("|        ITEM SHOP        ");
-            Console.WriteLine("+------------------------+");
-            Console.Write("How many items would you like to purchase? ");
-            bool isValidInput = int.TryParse(Console.ReadLine(), out int quantity);
-            Console.WriteLine($"Valid input: {isValidInput}");
-            Console.WriteLine($"You have purchased {quantity} items.");
-           
-            Console.WriteLine("+------------------------+");
-            Console.WriteLine("|     RATE THIS LEVEL     ");
-            Console.WriteLine("+------------------------+");
-            Console.Write("Rate this level (1-5): ");
-            bool isValidRating = int.TryParse(Console.ReadLine(), out int rating);
-            Console.WriteLine("Valid rating: " + isValidRating);
-            Console.WriteLine($"You have rated this level {rating}/5.");
-
-            Console.WriteLine("+------------------------+");
-            Console.WriteLine("|     CHARACTER CREATION  ");
+            Console.WriteLine("|   CHARACTER CREATION    ");
             Console.WriteLine("+------------------------+");
             Console.Write("Name your character: ");
             string CharacterName = Console.ReadLine();
@@ -53,6 +22,33 @@ namespace Lab04
             Console.WriteLine("Starting luck (1.0 - 10.0) ");
             bool isValidLuck = double.TryParse(Console.ReadLine(), out double luck);
             Console.WriteLine($"Character Name: {CharacterName}, Class: {classNum}, Luck: {luck}");
+
+            Console.WriteLine("+------------------------+");
+            Console.WriteLine("|        ITEM SHOP        ");
+            Console.WriteLine("+------------------------+");
+            Console.Write("How many items would you like to purchase? ");
+            bool isValidInput = int.TryParse(Console.ReadLine(), out int quantity);
+            Console.WriteLine($"Valid input: {isValidInput}");
+            Console.WriteLine($"You have purchased {quantity} items.");
+
+            Console.WriteLine("+------------------------+");
+            Console.WriteLine("|        SET VOLUME       ");
+            Console.WriteLine("+------------------------+");
+            Console.Write("Set your mic volume(0.0-1.0): ");
+            bool isValidVolume = double.TryParse(Console.ReadLine(), out double volume);
+            Console.WriteLine($"Valid input: {isValidVolume}");
+            Console.WriteLine($"Volume set to: {volume}");
+
+            Console.WriteLine("+------------------------+");
+            Console.WriteLine("|      NEW SAVE FILE      ");
+            Console.WriteLine("+------------------------+");
+            Console.Write("Name your save file: ");
+            string SaveName = Console.ReadLine();
+            Console.Write("Save slot (1-3): ");
+            bool isValidSlot = int.TryParse(Console.ReadLine(), out int slotNum);
+            Console.WriteLine($"Done! ,you have saved \"{SaveName}\" on slot {slotNum}");
+            Console.WriteLine($"Valid input: {isValidSlot}");
+         
         }
     }
 }
